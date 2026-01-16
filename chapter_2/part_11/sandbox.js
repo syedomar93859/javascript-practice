05a435836c93019eeb8525ecfd8bdd7d507aea6c
Syed Omar <syed.omar@ucalgary.ca>
2026-01-16 15:39:59 -0700

    finishing chapter 2

---
+++ chapter_2/part_11/sandbox.js
@@ -0,0 +1,16 @@
+// type conversion
+let score = '100';
+
+// score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let result = Number('hello');
+// let result = String(50);
+// let result = Boolean(0)
+
+// strings of any length are truthy expect for empty strings
+// let result = Boolean('0')
+let result = Boolean('');
+
+console.log(result, typeof result);
---
+++ chapter_2/part_9/sandbox.js
@@ -0,0 +1,32 @@
+// booleans & operators
+console.log(true, false, "true", "false");
+
+// methods can return booleans
+let email = "billybob@gmail.com";
+let names = ['mario','luigi','toad'];
+
+// let result = email.includes("@");
+// let result = email.includes("!");
+// let result = names.includes('bowser');
+// console.log(result);
+
+// comparison operators
+let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30); 
+// console.log(age != 30);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+let name = 'shaun';
+
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+// later letters in the alphabet are greater than earlier letters
+console.log(name > 'crystal');
+// lowercase letters are greater than uppercase letters
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');
